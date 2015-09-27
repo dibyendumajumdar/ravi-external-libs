53c28cf270cb83d8fc99ae9f100e18380a74830e
dibyendumajumdar <nomail>
2015-09-27 16:02:29 +0100

    env script path

--- bin/win64/ravienv.bat
+++ bin/win64/ravienv.bat
@@ -1,5 +1,5 @@
 set RAVI_HOME=c:\ravi
-set PATH=%PATH%;%RAVI_HOME%\bin
-set LUA_PATH=.\?.lua;.\lua\?.lua
+set PATH=%RAVI_HOME%\bin;%PATH%
+set LUA_PATH=.\?.lua;.\lua\?.lua;%RAVI_HOME%\lua\?.lua
 set LUA_CPATH=%RAVI_HOME%\bin\?.dll
 
