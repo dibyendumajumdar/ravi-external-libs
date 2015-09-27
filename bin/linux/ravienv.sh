c4c2d519962a600df9858503a9d865a9bf72d02c
dibyendumajumdar <nomail>
2015-09-27 15:21:23 +0100

    add scripts

---
+++ bin/linux/ravienv.sh
@@ -0,0 +1,6 @@
+export RAVI_HOME=$HOME/ravi
+export PATH=$RAVI_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$RAVI_HOME/bin:$LD_LIBRARY_PATH
+export LUA_PATH="./?.lua;./lua/?.lua"
+export LUA_CPATH="$RAVI_HOME/bin/?.so;$RAVI_HOME/bin/lib?.so"
+
---
+++ bin/win64/ravienv.bat
@@ -0,0 +1,5 @@
+set RAVI_HOME=c:\ravi
+set PATH=%PATH%;%RAVI_HOME%\bin
+set LUA_PATH=.\?.lua;.\lua\?.lua
+set LUA_CPATH=%RAVI_HOME%\bin\?.dll
+
