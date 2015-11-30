f67b6a1380f8f6851acca6dbcf141c960f643829
Dibyendu Majumdar <noemail>
2015-11-30 23:14:45 +0000

    change path

--- bin/osx/ravienv.sh
+++ bin/osx/ravienv.sh
@@ -1,6 +1,6 @@
 export RAVI_HOME=$HOME/ravi
 export PATH=$RAVI_HOME/bin:$PATH
-export DYLD_LIBRARY_PATH=$RAVI_HOME/bin:$DYLD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$RAVI_HOME/lib:$DYLD_LIBRARY_PATH
 export LUA_PATH="./?.lua;./lua/?.lua"
 export LUA_CPATH="$RAVI_HOME/bin/?.dylib;$RAVI_HOME/bin/lib?.dylib"
 
