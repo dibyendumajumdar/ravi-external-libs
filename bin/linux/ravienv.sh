6a371f0b41c8bece5815909d87d06df70da5ff8a
dibyendumajumdar <nomail>
2015-12-05 10:56:30 +0000

    move libs to lib folder

--- bin/linux/ravienv.sh
+++ bin/linux/ravienv.sh
@@ -2,5 +2,5 @@ export RAVI_HOME=$HOME/ravi
 export PATH=$RAVI_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$RAVI_HOME/lib:$LD_LIBRARY_PATH
 export LUA_PATH="./?.lua;./lua/?.lua"
-export LUA_CPATH="$RAVI_HOME/bin/?.so;$RAVI_HOME/bin/lib?.so"
+export LUA_CPATH="$RAVI_HOME/lib/?.so;$RAVI_HOME/lib/lib?.so"
 
--- bin/osx/ravienv.sh
+++ bin/osx/ravienv.sh
@@ -2,5 +2,5 @@ export RAVI_HOME=$HOME/ravi
 export PATH=$RAVI_HOME/bin:$PATH
 export DYLD_LIBRARY_PATH=$RAVI_HOME/lib:$DYLD_LIBRARY_PATH
 export LUA_PATH="./?.lua;./lua/?.lua"
-export LUA_CPATH="$RAVI_HOME/bin/?.dylib;$RAVI_HOME/bin/lib?.dylib"
+export LUA_CPATH="$RAVI_HOME/lib/?.dylib;$RAVI_HOME/lib/lib?.dylib"
 
