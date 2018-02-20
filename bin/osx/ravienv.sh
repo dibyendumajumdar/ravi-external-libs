14c962619f6d026b3594be842c0bfe86d80c733b
Dibyendu Majumdar <dibyendumajumdar@users.noreply.github.com>
2018-02-20 23:19:37 +0000

    Update ravienv.sh

--- bin/osx/ravienv.sh
+++ bin/osx/ravienv.sh
@@ -1,6 +1,6 @@
 export RAVI_HOME=$HOME/ravi
 export PATH=$RAVI_HOME/bin:$PATH
 export DYLD_LIBRARY_PATH=$RAVI_HOME/lib:$DYLD_LIBRARY_PATH
-export LUA_PATH="./?.lua;./lua/?.lua"
+export LUA_PATH="$RAVI_HOME/share/lua/5.3/?.lua;$RAVI_HOME/share/lua/5.3/?/init.lua;./?.lua;./?/init.lua"
 export LUA_CPATH="$RAVI_HOME/lib/?.dylib;$RAVI_HOME/lib/lib?.dylib"
 
