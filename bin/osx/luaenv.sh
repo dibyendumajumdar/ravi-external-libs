06cdaf50b05d09713a8f4ca7b92bb61d551f0d51
Dibyendu Majumdar <noemail>
2018-05-14 16:32:23 +0100

    add Lua env script

---
+++ bin/osx/luaenv.sh
@@ -0,0 +1,6 @@
+export LUA_HOME=$HOME/lua53
+export PATH=$LUA_HOME/bin:$PATH
+export DYLD_LIBRARY_PATH=$LUA_HOME/lib:$DYLD_LIBRARY_PATH
+export LUA_PATH="$LUA_HOME/share/lua/5.3/?.lua;$LUA_HOME/share/lua/5.3/?/init.lua;./?.lua;./?/init.lua"
+export LUA_CPATH="$LUA_HOME/lib/?.dylib;$LUA_HOME/lib/lib?.dylib"
+
---
+++ bin/win64/luaenv.bat
@@ -0,0 +1,5 @@
+set LUA_HOME=c:\Software\lua53
+set PATH=%LUA_HOME%\bin;%PATH%
+set LUA_PATH=%LUA_HOME%\share\lua\5.3\?.lua;%LUA_HOME%\share\lua\5.3\?\init.lua;.\?.lua;.\?\init.lua
+set LUA_CPATH=%LUA_HOME%\bin\?.dll
+
