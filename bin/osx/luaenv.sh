3314555492c05ef501cab4e2f36d5ed8af821790
Dibyendu Majumdar <dibyendumajumdar@users.noreply.github.com>
2018-11-10 16:38:33 +0000

    Update luaenv.sh

--- bin/osx/luaenv.sh
+++ bin/osx/luaenv.sh
@@ -3,4 +3,4 @@ export PATH=$LUA_HOME/bin:$PATH
 export DYLD_LIBRARY_PATH=$LUA_HOME/lib:$DYLD_LIBRARY_PATH
 export LUA_PATH="$LUA_HOME/share/lua/5.3/?.lua;$LUA_HOME/share/lua/5.3/?/init.lua;./?.lua;./?/init.lua"
 export LUA_CPATH="$LUA_HOME/lib/?.dylib;$LUA_HOME/lib/lib?.dylib"
-
+export LUA_CPATH="$LUA_CPATH:$LUA_HOME/lib/?.so;$LUA_HOME/lib/lib?.so"
