ea98135eb8dc946ff3ed9fa95699c9c69d36e5a5
Dibyendu Majumdar <dibyendumajumdar@users.noreply.github.com>
2020-01-18 21:34:20 +0000

    Delete ravienv.bat

--- bin/win64/ravienv.bat
+++
@@ -1,5 +0,0 @@
-set RAVI_HOME=c:\Software\ravi
-set PATH=%RAVI_HOME%\bin;%PATH%
-set LUA_PATH=%RAVI_HOME%\share\lua\5.3\?.lua;%RAVI_HOME%\share\lua\5.3\?\init.lua;.\?.lua;.\?\init.lua
-set LUA_CPATH=%RAVI_HOME%\bin\?.dll
-
