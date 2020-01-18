edf13264336c5bd23553cec4f802939e1075c491
Dibyendu Majumdar <dibyendumajumdar@users.noreply.github.com>
2020-01-18 21:33:50 +0000

    Delete ravienv.sh

--- bin/osx/ravienv.sh
+++
@@ -1,6 +0,0 @@
-export RAVI_HOME=$HOME/ravi
-export PATH=$RAVI_HOME/bin:$PATH
-export DYLD_LIBRARY_PATH=$RAVI_HOME/lib:$DYLD_LIBRARY_PATH
-export LUA_PATH="$RAVI_HOME/share/lua/5.3/?.lua;$RAVI_HOME/share/lua/5.3/?/init.lua;./?.lua;./?/init.lua"
-export LUA_CPATH="$RAVI_HOME/lib/?.dylib;$RAVI_HOME/lib/lib?.dylib"
-export LUA_CPATH="$LUA_CPATH;$RAVI_HOME/lib/?.so;$RAVI_HOME/lib/lib?.so"
