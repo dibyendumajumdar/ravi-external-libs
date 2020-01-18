02c4f0718a6a7b21e07ca52e318df3a5ed843315
Dibyendu Majumdar <dibyendumajumdar@users.noreply.github.com>
2020-01-18 21:33:00 +0000

    Delete luaenv.sh

--- bin/linux/luaenv.sh
+++
@@ -1,6 +0,0 @@
-export LUA_HOME=$HOME/lua53
-export PATH=$LUA_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$LUA_HOME/lib:$LD_LIBRARY_PATH
-export LUA_PATH="$LUA_HOME/share/lua/5.3/?.lua;$LUA_HOME/share/lua/5.3/?/init.lua;./?.lua;./?/init.lua"
-export LUA_CPATH="$LUA_HOME/lib/?.so;$LUA_HOME/lib/lib?.so"
-
